97a8ffcbc2669307147fbca3120795795e5afca7
wonkothesanest <dustin.hagstrom@gmail.com>
2023-09-14 17:13:50 -0700

    Finally we have services that startup. Cool system linux bro

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,13 @@ ROOT_DIR=$(dirname $(dirname $(realpath $0)))
 echo "[Unit]
 Description=Audio Porcupine Keyword Spotter
 After=pulseaudio.service
+Requires=pulseaudio.service
 
 [Service]
 ExecStart=${ROOT_DIR}/python/audio_porcupine_keyword_spotter.py
-User=pulse
+User=pi
+Restart=always
+RestartSec=2
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/audio_porcupine_keyword_spotter.service
@@ -19,20 +22,28 @@ WantedBy=multi-user.target" | sudo tee /etc/systemd/system/audio_porcupine_keywo
 echo "[Unit]
 Description=Audio to Pulse Audio
 After=pulseaudio.service
+Requires=pulseaudio.service
 
 [Service]
 ExecStart=${ROOT_DIR}/python/audio_to_pulse_audio.py
-User=pulse
+User=pi
+Restart=always
+RestartSec=2
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/audio_to_pulse_audio.service
 
 echo "[Unit]
 Description=PulseAudio system server
+After=networking.service
+Requires=networking.service
 
 [Service]
 Type=simple
-ExecStart=/usr/bin/pulseaudio --system --disallow-exit --disable-shm --exit-idle-time=-1
+ExecStart=/usr/bin/pulseaudio --disallow-exit=true --disable-shm=true --exit-idle-time=-1
+User=pi
+Restart=always
+RestartSec=2
 
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/pulseaudio.service
